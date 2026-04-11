636971af9edc72010eb02c503bbc4e7188a166d8
Christopher Hutchinson <chris.hutchinson008@gmail.com>
2026-04-11 20:20:16 +0100

    add projects page route to app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route('/projects')
+def projects_page():
+    return render_template("projects.html")
+
 if __name__ == '__main__':
     app.run()
