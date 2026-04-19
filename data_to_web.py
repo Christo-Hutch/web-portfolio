4db0d37b374f410fcae40f86cca9c0de09dc529d
Christo-Hutch <chris.hutchinson008@gmail.com>
2026-04-19 23:18:34 +0100

    add strip function within creation of details dict

--- data_to_web.py
+++ data_to_web.py
@@ -8,6 +8,6 @@ def get_details():
     DETAILS = {}
 
     for i in range(0, len(details_list) // 2, 2):
-        DETAILS[details_list[i]] = details_list[i + 1]
+        DETAILS[details_list[i].strip()] = details_list[i + 1].strip()
 
     return DETAILS
